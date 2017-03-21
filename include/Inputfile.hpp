aa03b3df167a1986c78ff4de89679e94676b9756
jacopodisi <disimone.jacopo@mail.polimi.it>
2017-03-21 23:56:18 +0100

    added the documentation

--- include/Inputfile.hpp
+++ include/Inputfile.hpp
@@ -4,48 +4,45 @@
 #include "Type.hpp"
 
 struct fileheaders {
-	 uint32_t numtraces;
-	 uint32_t numsamples_per_trace;
-	 char datatype;
-	 uint8_t knowndatalength;
+   uint32_t numtraces;
+   uint32_t numsamples_per_trace;
+   char datatype;
+   uint8_t knowndatalength;
 };
 
 class Inputfile{
    int inputfd;
    fileheaders header;
-	 long long RealFileSize;
-	 void* fileoffset;
+   long long RealFileSize;
+   void* fileoffset;
    bool fl; //true if the trace is saved in float, false if it is save in double
 
 public:
-   /* apre il file "filename" e salva:
-    * RealFileSize
-    * fileoffset
-    * SamplesPerTrace
-    * NumTraces
+   /* Inputfile constructor: create the object initializing private variables
+    * and mapping fileoffset.
     */
    Inputfile(const char * filename);
-   //void closefile();
-   /* legge tutti i ptx e li salva in data che è un puntatore a un vettore
-    * di DATA_SIZE_BIT (DataValueType)
+   /* read some samples present in the file and save them in a TracesMatrix passed
+    * as parameter, in the row traces_row.
     */
-   std::shared_ptr<DataMatrix> readData();
-   void readSamples ( std::shared_ptr<TracesMatrix>& traces, unsigned long it, unsigned long trace, unsigned long startingsample, unsigned long numsamples );
-   void readPtx ( std::shared_ptr<DataMatrix>& data_matrix, unsigned long it, unsigned long trace);
-	 unsigned long getNumSamplesPerTrace();
-	 unsigned long getNumTraces();
-	 unsigned long getNumPtx();
+   void readSamples ( std::shared_ptr<TracesMatrix> traces, unsigned long traces_row, unsigned long trace, unsigned long startingsample, unsigned long numsamples );
+   /* read the data of a given trace and save it in data_matrix in the row specified
+    * by parameter data_row
+    */
+   void readPtx ( std::shared_ptr<DataMatrix> data_matrix, unsigned long data_row, unsigned long trace );
+   unsigned long getNumSamplesPerTrace();
+   unsigned long getNumTraces();
+   unsigned long getNumPtx();
 
    ~Inputfile();
 
 protected:
    unsigned long long getSampleOffset ( unsigned long trace, unsigned long samplenum ) {
-       return HEADER_SIZE + trace * ( SAMPLE_SIZE * header.numsamples_per_trace + DATA_SIZE_BYTE * 16) + SAMPLE_SIZE * samplenum;
+      return HEADER_SIZE + trace * ( SAMPLE_SIZE * header.numsamples_per_trace + 16) + SAMPLE_SIZE * samplenum;
    }
    unsigned long long getDataOffset ( unsigned long trace ) {
-       return HEADER_SIZE + trace * ( SAMPLE_SIZE * header.numsamples_per_trace + DATA_SIZE_BYTE * 16) + SAMPLE_SIZE * header.numsamples_per_trace;
+      return HEADER_SIZE + trace * ( SAMPLE_SIZE * header.numsamples_per_trace + 16) + SAMPLE_SIZE * header.numsamples_per_trace;
    }
-
 };
 
 #endif //INPUTFILE
--- include/Opanalysis.hpp
+++ include/Opanalysis.hpp
@@ -9,20 +9,40 @@ class Opanalysis{
 
 public:
 
-   static void aesModelHW(std::shared_ptr<TracesMatrix>& pModel, std::shared_ptr<DataMatrix>& data_matrix, unsigned long numtraces, unsigned long colptx, char out_model);
+   /* Compute the Hamming Weight model of the byte "byte" of the given data_matrix, 
+    * using the parameter aes_model as an option to choose the model at the output
+    * of the first sbox using 'S', or, at the first add round key, using 'K'.
+    */
+   static void aesModelHW(std::shared_ptr<TracesMatrix> pModel, std::shared_ptr<DataMatrix> data_matrix, unsigned long numtraces, unsigned long byte, char aes_model);
 
+   /* Apply the Optimal Distinguisher for known model, saving the traces (as they are)
+    * in a file, saving the finded key in another file and displaying it.
+    * All these actions depend on option passed as parameter to the function.
+    * To find the key, the algorithm compute, for each sample, the mean of the results 
+    * for each key, the new results will be the difference of the old results with
+    * the mean.
+    */
+   static void DoMKnownmodel(std::shared_ptr<TracesMatrix> traces_matrix, std::shared_ptr<TracesMatrix> power_model, unsigned long startingsample, unsigned long numsamples, unsigned long numtraces, char noiseAssumption, char aes_model, int byte, bool save, bool savek);
 
-   static void DoMKnownmodel(std::shared_ptr<TracesMatrix>& traces_matrix, std::shared_ptr<TracesMatrix>& power_model, unsigned long startingsample, unsigned long numsamples, unsigned long ntraces, char noiseAssumption, char out_model, int ptx, bool save, bool savek);
+   /* Apply the optimal distinguisher for partially known model, saving the traces 
+    * (as they are) in a file, saving the finded key in an other file and displaying it.
+    * All these action depend on option passed as parameter.
+    * To retrieve the best key it find the argmax for each sample and take the one
+    * which appear more times.
+    */
+   static void DoMUnknownmodel(std::shared_ptr<TracesMatrix> traces_matrix, std::shared_ptr<TracesMatrix> power_model, unsigned long startingsample, unsigned long numsamples, unsigned long numtraces, char aes_model, double pel_coeff, double plat_noise, int byte, bool save, bool savek);
 
-   static void DoMUnknownmodel(std::shared_ptr<TracesMatrix>& traces_matrix, std::shared_ptr<TracesMatrix>& power_model, unsigned long startingsample, unsigned long numsamples, unsigned long ntraces, char out_model, double pel_coeff, double plat_noise, int ptx, bool save, bool savek);
+   /* Apply the correlation power analysis, saving the traces 
+    * in a file, saving the finded key in an other file and displaying it.
+    * All these action depend on option passed as parameter.
+    */
+   static void CPA(std::shared_ptr<TracesMatrix> traces_matrix, std::shared_ptr<TracesMatrix> power_model, unsigned long startingsample, unsigned long numsamples, unsigned long numtraces, char aes_model, int byte, bool save, bool savek);
 
-   static void CPA(std::shared_ptr<TracesMatrix>& traces_matrix, std::shared_ptr<TracesMatrix>& power_model, unsigned long startingsample, unsigned long numsamples, unsigned long ntraces, char out_model, int ptx, bool save, bool savek);
-
-   static void saveInFile(std::shared_ptr<TracesMatrix>& arg, std::string filename, std::string directoryname, unsigned long numsam)
+   static void saveInFile(std::shared_ptr<TracesMatrix> results, std::string filename, std::string directoryname, unsigned long numsamples)
    {
       std::string dirname = "./" + directoryname;
       mkdir(dirname.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-      double output[numsam];
+      double output[numsamples];
       std::string fn = dirname + "/" + filename;
       FILE *fs = fopen(fn.c_str(), "wb");
       if(!fs) {
@@ -30,11 +50,11 @@ public:
       }
       for (size_t i = 0; i < KEY_SIZE; i++)
       {
-         for (size_t j = 0; j < numsam; j++)
+         for (size_t j = 0; j < numsamples; j++)
          {
-            output[j] = (*arg)(i,j);
+            output[j] = (*results)(i,j);
          }
-         std::fwrite(output, sizeof(double), numsam, fs);
+         std::fwrite(output, sizeof(double), numsamples, fs);
          if(std::fflush(fs) != 0)
          {
             std::cout << "Error in flushing file" << '\n';
